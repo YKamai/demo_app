9e2d9d68120a48ceac6abad95569194473016a44
Yauhenka <yauhenka@gmail.com>
2017-02-15 23:29:46 +0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+ruby '2.2.3'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.2'
+
+group :development do
+  gem 'byebug'
+  gem 'sqlite3', '1.3.13'
+end
+
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jbuilder', '~> 1.2'
+gem 'jquery-rails'
+gem 'sass-rails', '~> 4.0.0'
+gem 'turbolinks'
+gem 'uglifier', '>= 1.3.0'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :production do
+  gem 'pg', '0.15.1'
+  gem 'rails_12factor', '0.0.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '4746d45acfc91fe3db46f8a1b80cb1289ed50dc5fb24878629fde6d207a5bbdbd6a68d0ea55d2286d79b8765836dd4256577199dbf1f45d58570cd717e9b1cf6'
